e2bb0a711d41ca808fb4afc9a0babded9c595db5
DarkBouffeur <farnham.tiihiva@gmail.com>
2019-06-03 16:18:13 +0200

    Ini  commit

---
+++ Source/Kensei/KenseiGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KenseiGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KENSEI_API AKenseiGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
